b5606cdc0b0ee861c07b8a4b67dc60355a9ef096
Alessandro Manica <alexman94@hotmail.it>
2020-07-14 11:24:20 +0200

    cambiata la generazione dell'id per avere lo stesso id della manutenzione

--- src/main/java/it/unisalento/server/entities/Chat.java
+++ src/main/java/it/unisalento/server/entities/Chat.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Chat {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @OneToOne
